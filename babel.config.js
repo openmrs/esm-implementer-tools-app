540d34839d4c1eb54f1669c4d1b34bfddc8ec939
Brandon Istenes <bistenes@gmail.com>
2020-09-17 10:08:31 -0700

    Remove 'target' declaration from babel config

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,7 @@
 module.exports = {
   presets: [
-    [
-      "@babel/preset-env",
-      {
-        targets: {
-          node: "current",
-        },
-      },
-    ],
+    "@babel/preset-env",
     "@babel/preset-typescript",
     "@babel/preset-react",
-  ],
-  plugins: [
-    "@babel/plugin-proposal-optional-chaining",
-    "@babel/plugin-proposal-nullish-coalescing-operator",
   ],
 };
