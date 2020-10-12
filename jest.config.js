1d4a88aaf1fb28b04f3a85d6db604865bdbfca63
Florian Rappl <florian.rappl@smapiot.com>
2020-10-12 17:39:00 +0200

    Consolidated and aligned repo structure

--- jest.config.js
+++ jest.config.js
@@ -8,10 +8,10 @@ module.exports = {
   moduleNameMapper: {
     "lodash-es": "lodash",
     "\\.(css)$": "identity-obj-proxy",
-    "@openmrs/esm-module-config":
+    "@openmrs/esm-config":
       "<rootDir>/__mocks__/openmrs-esm-module-config.mock.tsx",
     "@openmrs/esm-api": "<rootDir>/__mocks__/openmrs-esm-api.mock.tsx",
-    "@openmrs/esm-extension-manager":
+    "@openmrs/esm-extensions":
       "<rootDir>/__mocks__/openmrs-esm-extension-manager.mock.tsx",
   },
 };
